0a10853b7e3ac6ca8f893907102c2da1fa340b43
Kevin Tran <kevin91801@yahoo.com>
2017-06-11 18:20:56 -0700

    hw

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD94g3vgHyOD4xbBTMqe5oQjZ8XQNBge3I",
+    authDomain: "first-project-205ab.firebaseapp.com",
+    databaseURL: "https://first-project-205ab.firebaseio.com",
+    projectId: "first-project-205ab",
+    storageBucket: "first-project-205ab.appspot.com",
+    messagingSenderId: "559792794861"
+  };
+    firebase.initializeApp(config);
